4ce0a979f3d64b20fa31e303503f699b7c22e70e
Kevin Bradford <k7gagnon@hotmail.com>
2024-04-21 09:33:58 -0400

    Add project files.

---
+++ SubtitleRenamerForPlex/ReadMe.md
@@ -0,0 +1,26 @@
+# SubtitleRenamerForPlex
+
+This program rename the subtitle files so Plex can recognized the language assossiated to the video.
+If you have multiple files with the same language, you can manually choose which one you want to keep and delete the other to be sure to keep the right one or this program will increment a number at the end. Plex may not recognized all files.
+
+The program will ask you two arguments. The path and the video name. Please keep the video name blank if the subtitile file name already contains the name of the video. Or you can pass the Path and video name in argument from the console.
+
+
+### Download and install
+```
+git clone git@github.com:Bradford1138/SubtitleRenamerForPlex.git
+cd SubtitleRenamerForPlex
+pip install -r requirements.txt
+```
+
+### Examples
+Example of subtitle files with the name of the video: \
+![alt text](Before_with_name.JPG "Before_with_name")
+![alt text](Console_with_name.JPG "Console_with_name")
+![alt text](After.JPG "After")
+\
+\
+Example of subtitle files without the name of the video: \
+![alt text](Before_without_name.JPG "Before_without_name")
+![alt text](Console_without_name.JPG "Console_without_name")
+![alt text](After.JPG "After")
---
+++ SubtitleRenamerForPlex/SubtitleRenamerForPlex.py
@@ -0,0 +1,50 @@
+from iso639 import Lang
+from iso639 import exceptions
+import os
+from pathlib import Path
+import re
+import sys
+
+sub_extensions = [".srt", ".ass", ".ssa", ".smi", ".vtt"]
+validation_recommended = False
+
+try:
+    directory = sys.argv[1]
+    video_name = sys.argv[2]
+except IndexError:  #If no argument is passed, ask for parameters
+    directory = input("Enter directory path (i.e. C:/myfolder/subs): ")
+    video_name = input("Enter the movie/episode file name. Just tap the Enter key if the subtitle files already have the movie/episode name in the file name: ")
+
+#debug
+#directory = "H:/rename/"
+#video_name = "Star.Wars.Tales.of.the.Jedi.S01E01.1080p.WEBRip.x265-RARBG[eztv.re]"
+
+if len(video_name) > 0:
+    video_name = video_name + "."
+
+for filename in filter(lambda f: f.endswith(tuple(sub_extensions)), os.listdir(directory)):
+    filename_incr = 1   #Initialize increment in case there is multiple file with the same language
+    lastDotIdx = Path(filename).stem.rfind(".") #Get the index of the last dot before the language
+    filename_1 = filename[:lastDotIdx + 1]  #First part of the file name to keep
+    
+    try:
+        #Greek have two ISO-639-2 codes, Modern(gre) and Ancient(grc)
+        if re.sub("[0-9]+_", "", Path(filename[lastDotIdx+1:]).stem) == "Greek":
+            lg = "gre"
+        else:
+            lg = Lang(re.sub("[0-9]+_", "", Path(filename[lastDotIdx+1:]).stem)).pt2b
+
+        new_filename = video_name + filename_1 + lg + Path(filename).suffix
+        while os.path.isfile(os.path.join(directory, new_filename)): #If the new file name already exists, add a number at the end
+            new_filename = video_name + filename_1 + lg + "." + str(filename_incr) + Path(filename).suffix
+            filename_incr += 1
+            validation_recommended = True
+
+        os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename))
+    except  exceptions.InvalidLanguageValue:
+        print("Only valid ISO 639 language values are supported as arguments. Language not found: " + re.sub("[0-9]+_", "", Path(filename[lastDotIdx+1:]).stem))
+
+if validation_recommended == True:
+    print("Please validate the subtitles, there may be multiple files with the same language. Plex may not recognized them all.")
+input("Press Enter to exit")
+    
