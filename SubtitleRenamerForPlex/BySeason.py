1bf5c999bdb42825664f7012586dc90167021554
Kevin Bradford <k7gagnon@hotmail.com>
2024-06-10 18:32:59 -0400

    Fix a bug when there there is a space in the path of the season

--- SubtitleRenamerForPlex/BySeason.py
+++ SubtitleRenamerForPlex/BySeason.py
@@ -4,11 +4,9 @@ from pathlib import Path
 root = input("Enter directory path (i.e. C:/myfolder/Season1): ")
 
 for file in os.listdir(root):
-    #print(file + " ; root: " + str(os.path.isdir(os.path.join(root, file))))
-    
     if (os.path.isdir(os.path.join(root, file))):
-        os.system("python SubtitleRenamerForPlex.py " + os.path.join(root, file) + " " + file + " True")
-    elif (os.path.exists(os.path.join(root, Path(file).stem)) and os.path.isdir(os.path.join(root, Path(file).stem))): #if the episode is outside the folder with his name, move the file inse the folder
+        os.system("python SubtitleRenamerForPlex.py " + "\"" + os.path.join(root, file) + "\" \"" + file + "\" True")
+    elif (os.path.exists(os.path.join(root, Path(file).stem)) and os.path.isdir(os.path.join(root, Path(file).stem))): #if the episode is outside the folder with his name, move the file inside the folder
         os.rename(os.path.join(root, file), os.path.join(root, Path(file).stem, file))
 
 input("Press Enter to exit")
